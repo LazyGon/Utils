f1cfacdc58189ca07a025bf92ff9671bf8de5edd
akaregi <akg.tachibana@gmail.com>
2019-02-28 02:56:11 +0900

    Initial Commit

---
+++ src/lazyutils/plugin/LazyUtilsConfig.java
@@ -0,0 +1,47 @@
+package lazyutils.plugin;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+public class LazyUtilsConfig {
+
+	private FileConfiguration config;
+
+	private String PermissionDenied;
+	private String FilerHomeDirectory;
+	private String FilerDownloadMessage;
+	private String FilerRemoveMessage;
+	private String FilerHelpMessage;
+
+	void loadConfig(){
+
+		config = LazyUtils.getInstance().getConfig();
+
+		this.PermissionDenied = config.getString("PermissionDenied");
+		this.FilerHomeDirectory = config.getString("HomeDirectory");
+		this.FilerDownloadMessage = config.getString("DownloadMessage");
+		this.FilerRemoveMessage = config.getString("RemoveMessage");
+		this.FilerHelpMessage = config.getString("HelpMessage");
+
+	}
+
+	public String getPermissionDenied() {
+		return this.PermissionDenied;
+	}
+
+	public String getFilerHomeDirectory() {
+		return this.FilerHomeDirectory;
+	}
+
+	public String getFilerDownloadMessage() {
+		return this.FilerDownloadMessage;
+	}
+
+	public String getFilerRemoveMessage() {
+		return this.FilerRemoveMessage;
+	}
+
+	public String getFilerHelpMessage() {
+		return this.FilerHelpMessage;
+	}
+
+}
